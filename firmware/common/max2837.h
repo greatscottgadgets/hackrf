61b8be45fec051b90ce26a09178273d2b2651deb
Will Code <willcode4@gmail.com>
2012-05-29 18:10:52 -0400

    max2837 driver under construction

---
+++ firmware/common/max2837.h
@@ -0,0 +1,35 @@
+#ifndef __MAX2837_H
+#define __MAX2837_H
+
+/* TODO - make this a private header for max2837.c only, make new max2837.h */
+
+/* 32 registers, each containing 10 bits of data. */
+#define MAX2837_NUM_REGS 32
+
+/* TODO - these externs will be local to max2837.c ... don't define here? */
+extern uint16_t max2837_regs[MAX2837_NUM_REGS];
+extern uint32_t max2837_regs_dirty;
+
+#define MAX2837_REG_SET_CLEAN(r) max2837_regs_dirty &= ~(1UL<<r)
+#define MAX2837_REG_SET_DIRTY(r) max2837_regs_dirty |= (1UL<<r)
+
+/* Initialize chip. */
+extern void max2837_init(void);
+
+/* Read a register via SPI. Save a copy to memory and return
+ * value. Mark clean. */
+extern uint16_t max2837_reg_read(uint8_t r);
+
+/* Write value to register via SPI and save a copy to memory. Mark
+ * clean. */
+extern void max2837_reg_write(uint8_t r, uint16_t v);
+
+/* Read all registers from chip and copy to memory. Mark all clean. */
+extern void max2837_regs_read(void);
+
+/* Write all dirty registers via SPI from memory. Mark all clean. Some
+ * operations require registers to be written in a certain order. Use
+ * provided routines for those operations. */
+extern void max2837_regs_commit(void);
+
+#endif // __MAX2837_H
