afdcd3f37e93f38e4bb8bbdab70041d4cc191f4e
Michael Ossmann <mike@ossmann.com>
2012-06-07 08:15:22 -0600

    migrated blinky.c to new libopencm3 stuff (copied from libopencm3 miniblink example)

--- firmware/blinky/blinky.c
+++ firmware/blinky/blinky.c
@@ -19,39 +19,67 @@
  * Boston, MA 02110-1301, USA.
  */
 
+#include <libopencm3/lpc43xx/gpio.h>
+#include <libopencm3/lpc43xx/scu.h>
+
 #include "hackrf_core.h"
 
-void wait(uint8_t duration)
+void gpio_setup(void)
 {
-	volatile uint32_t i;
-	for (i = 0; i < duration * 1000000; i++);
+	/* Configure SCU Pin Mux as GPIO */
+	scu_pinmux(SCU_PINMUX_LED1, SCU_GPIO_FAST);
+	scu_pinmux(SCU_PINMUX_LED2, SCU_GPIO_FAST);
+	scu_pinmux(SCU_PINMUX_LED3, SCU_GPIO_FAST);
+
+	scu_pinmux(SCU_PINMUX_EN1V8, SCU_GPIO_FAST);
+
+	scu_pinmux(SCU_PINMUX_BOOT0, SCU_GPIO_FAST);
+	scu_pinmux(SCU_PINMUX_BOOT1, SCU_GPIO_FAST);
+	scu_pinmux(SCU_PINMUX_BOOT2, SCU_GPIO_FAST);
+	scu_pinmux(SCU_PINMUX_BOOT3, SCU_GPIO_FAST);
+
+	/* Configure all GPIO as Input (safe state) */
+	GPIO0_DIR = 0;
+	GPIO1_DIR = 0;
+	GPIO2_DIR = 0;
+	GPIO3_DIR = 0;
+	GPIO4_DIR = 0;
+	GPIO5_DIR = 0;
+	GPIO6_DIR = 0;
+	GPIO7_DIR = 0;
+
+	/* Configure GPIO2[1/2/8] (P4_1/2 P6_12) as output. */
+	GPIO2_DIR |= (PIN_LED1|PIN_LED2|PIN_LED3);
+
+	/* GPIO3[6] on P6_10 as output. */
+	GPIO3_DIR |= PIN_EN1V8;
 }
 
-uint32_t boot0, boot1, boot2, boot3;
+u32 boot0, boot1, boot2, boot3;
 
-int main()
+int main(void)
 {
+	int i;
+	gpio_setup();
 
-	gpio_init();
+	/* Set 1V8 */
+	gpio_set(PORT_EN1V8, PIN_EN1V8);
 
-	EN1V8_SET;
-	EN1V8_CLR;
+	/* Blink LED1/2/3 on the board and Read BOOT0/1/2/3 pins. */
+	while (1) 
+	{
+		boot0 = BOOT0_STATE;
+		boot1 = BOOT1_STATE;
+		boot2 = BOOT2_STATE;
+		boot3 = BOOT3_STATE;
 
-	while (1) {
-	    boot0 = BOOT0;
-	    boot1 = BOOT1;
-	    boot2 = BOOT2;
-	    boot3 = BOOT3;
-
-		LED1_SET;
-		LED2_SET;
-		LED3_SET;
-		wait(1);
-		LED1_CLR;
-		LED2_CLR;
-		LED3_CLR;
-		wait(1);
+		gpio_set(PORT_LED1_3, (PIN_LED1|PIN_LED2|PIN_LED3)); /* LEDs on */
+		for (i = 0; i < 2000000; i++)	/* Wait a bit. */
+			__asm__("nop");
+		gpio_clear(PORT_LED1_3, (PIN_LED1|PIN_LED2|PIN_LED3)); /* LED off */
+		for (i = 0; i < 2000000; i++)	/* Wait a bit. */
+			__asm__("nop");
 	}
 
-	return 0 ;
+	return 0;
 }
