02f61f4d64c9f0be0f945bc0287f22f0fd236a77
Jared Boone <jboone@earfeast.com>
2012-06-13 17:53:10 -0700

    Added r_div argument to si5351c_configure_multisynth(). Modified Jellybean clock setup to provide 10MHz clock to MAX5864 and 20MHz to CPLD (both inverted and non-inverted).

--- firmware/common/si5351c.h
+++ firmware/common/si5351c.h
@@ -40,7 +40,8 @@ void si5351c_enable_xo_and_ms_fanout();
 void si5351c_configure_pll_sources_for_xtal();
 void si5351c_configure_pll1_multisynth();
 void si5351c_configure_multisynth( const uint_fast8_t ms_number,
-    	const uint32_t p1, const uint32_t p2, const uint32_t p3);
+    	const uint32_t p1, const uint32_t p2, const uint32_t p3,
+    	const uint_fast8_t r_div);
 void si5351c_configure_clock_control();
 void si5351c_enable_clock_outputs();
 
