e29ec6b0849cb9e9510cbd03a7683e88d5da9061
Jared Boone <jboone@earfeast.com>
2013-11-20 15:09:52 -0800

    Add default M0 code that just loops forever, if a project doesn't specify any SRC_M0_[CS] files.

---
+++ firmware/common/m0_sleep.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2013 Jared Boone <jared@sharebrained.com>
+ *
+ * This file is part of HackRF.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+int main() {
+	while(1) {
+		
+	}
+}
