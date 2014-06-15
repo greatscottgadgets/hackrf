9e95d235b436b7bd4132cf72296fb4504eb76b35
Jared Boone <jboone@earfeast.com>
2014-06-15 15:26:20 -0700

    Create separate CMake toolchain file. Add references to toolchain file in each firmware CMakeLists.txt. I suspect this can be simpler still, just not sure how, yet...

--- firmware/hackrf-common.cmake
+++ firmware/hackrf-common.cmake
@@ -24,43 +24,7 @@
 
 # derived primarily from Makefiles in libopencm3
 
-cmake_minimum_required(VERSION 2.8.9)
-
 enable_language(C CXX ASM)
-
-include(CMakeForceCompiler)
-
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
-
-CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
-CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
-
-execute_process(
-  COMMAND ${CMAKE_C_COMPILER} -print-file-name=libc.a
-  OUTPUT_VARIABLE CMAKE_INSTALL_PREFIX
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-get_filename_component(CMAKE_INSTALL_PREFIX
-  "${CMAKE_INSTALL_PREFIX}" PATH
-)
-get_filename_component(CMAKE_INSTALL_PREFIX
-  "${CMAKE_INSTALL_PREFIX}/.." REALPATH
-)
-set(CMAKE_INSTALL_PREFIX  ${CMAKE_INSTALL_PREFIX} CACHE FILEPATH
-    "Install path prefix, prepended onto install directories.")
-
-message(STATUS "Cross-compiling with the gcc-arm-embedded toolchain")
-message(STATUS "Toolchain prefix: ${CMAKE_INSTALL_PREFIX}")
-
-set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
-#set(CMAKE_LD ${CMAKE_INSTALL_PREFIX}/bin/ld CACHE INTERNAL "ld tool")
-set(CMAKE_OBJCOPY ${CMAKE_INSTALL_PREFIX}/bin/objcopy CACHE INTERNAL "objcopy tool")
-
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_INSTALL_PREFIX})
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 SET(PATH_HACKRF ../..)
 SET(PATH_HACKRF_FIRMWARE ${PATH_HACKRF}/firmware)
