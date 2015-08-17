69db61c5c1e06b295fc1d5d0b4f268b691d6d8fe
Jared Boone <jboone@earfeast.com>
2015-08-17 11:14:55 -0700

    Remove extra SGPIO API calls.
    Functions are now performed in rf_path_set_direction().

--- firmware/sgpio-rx/sgpio-rx.c
+++ firmware/sgpio-rx/sgpio-rx.c
@@ -29,9 +29,6 @@
 #include <tuning.h>
 
 void tx_test() {
-	sgpio_set_slice_mode(false);
-	sgpio_configure(TRANSCEIVER_MODE_TX);
-	
 	// LSB goes out first, samples are 0x<Q1><I1><Q0><I0>
 	volatile uint32_t buffer[] = {
 		0xda808080,
@@ -51,9 +48,6 @@ void tx_test() {
 }
 
 void rx_test() {
-	sgpio_set_slice_mode(false);
-	sgpio_configure(TRANSCEIVER_MODE_RX);
-
     volatile uint32_t buffer[4096];
     uint32_t i = 0;
 	uint32_t magsq;
