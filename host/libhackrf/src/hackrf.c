4f9665fb3b43462e39a1592fc34f3dfb50de4a07
Vinayak Kariappa Chettimada <vinayak.kariappa@gmail.com>
2016-04-03 07:36:48 +0200

    Fixed missing reset of do_exit, which prevented calls to callback when thread is created once again.

--- host/libhackrf/src/hackrf.c
+++ host/libhackrf/src/hackrf.c
@@ -1415,6 +1415,7 @@ static int create_transfer_thread(hackrf_device* device,
 	if( device->transfer_thread_started == false )
 	{
 		device->streaming = false;
+		do_exit = false;
 
 		result = prepare_transfers(
 			device, endpoint_address,
