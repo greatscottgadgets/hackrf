e3a3bffca58180adf49612bd600c9e7f705c4dbb
Michael Ossmann <mike@ossmann.com>
2017-01-26 22:21:18 -0700

    fixed bug in dfu.py that made it only work for hackrf_usb

--- firmware/dfu.py
+++ firmware/dfu.py
@@ -1,7 +1,8 @@
 import os.path
 import struct
+import sys
 
 with open("_header.bin", "wb") as f:
-    x = struct.pack('<H', os.path.getsize('hackrf_usb_dfu.bin') // 512 + 1)
+    x = struct.pack('<H', os.path.getsize(sys.argv[1] + '_dfu.bin') // 512 + 1)
     y = [0xda, 0xff, x[0], x[1], 0xff, 0xff, 0xff, 0xff]
     f.write(bytearray(y))
--- firmware/hackrf-common.cmake
+++ firmware/hackrf-common.cmake
@@ -194,7 +194,7 @@ macro(DeclareTargets)
 		COMMAND rm -f _tmp.dfu _header.bin
 		COMMAND cp ${PROJECT_NAME}_dfu.bin _tmp.dfu
 		COMMAND ${DFU_COMMAND}
-		COMMAND python ../../dfu.py
+		COMMAND python ../../dfu.py ${PROJECT_NAME}
 		COMMAND cat _header.bin _tmp.dfu >${PROJECT_NAME}.dfu
 		COMMAND rm -f _tmp.dfu _header.bin
 	)
