0498a4a6c27e4fdec881f1a69e44312d94c35f18
Dominic Spill <dominicgs@gmail.com>
2018-12-04 15:06:12 -0700

    Try changing pip install path

--- firmware/appveyor.sh
+++ firmware/appveyor.sh
@@ -4,8 +4,8 @@ pwd
 echo $PYTHONPATH
 /usr/bin/env python -V
 /usr/bin/env python -c "import sys; print sys.path"
-pip install pyyaml
-pip2 install pyyaml
+pip install --install-option="--prefix=/usr/lib/python2.7/site-packages" pyyaml
+# pip2 install pyyaml
 ls /usr/lib/python2.7/site-packages
 cd firmware/libopencm3
 make
