ff82df34770de82461d266d3d33ccfaa0f7d200d
Dominic Spill <dominicgs@gmail.com>
2018-12-05 14:44:53 -0700

    Show make help text

--- firmware/appveyor.sh
+++ firmware/appveyor.sh
@@ -4,7 +4,8 @@
 /usr/bin/env python -m pip install pyyaml
 
 cd firmware/libopencm3
-PREFIX=/cygdrive/c/gcc-arm-none-eabi/bin/arm-none-eabi make lib/lpc43xx/m0
+make -h
+make lib/lpc43xx/m0
 ls -l /cygdrive/c/projects/hackrf/firmware/libopencm3/lib/
 make lib/lpc43xx/m4
 cd ..
