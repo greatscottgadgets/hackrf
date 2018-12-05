58e87408b1bb1a4d40d789a5a3aab6690b5a6e43
Dominic Spill <dominicgs@gmail.com>
2018-12-05 15:18:39 -0700

    Try some other pathing stuff

--- firmware/appveyor.sh
+++ firmware/appveyor.sh
@@ -4,7 +4,7 @@
 /usr/bin/env python -m pip install pyyaml
 
 cd firmware/libopencm3
-arm-none-eabi-ar -h
+export SRCLIBDIR="c:\projects\hackrf\firmware\libopencm3\lib\""
 make V=1 lib/lpc43xx/m0
 ls -l /cygdrive/c/projects/hackrf/firmware/libopencm3/lib/
 make V=1 lib/lpc43xx/m4
