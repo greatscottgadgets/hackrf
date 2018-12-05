91b3306950eb1dfce354b4bd65c93aae0b9ac024
Dominic Spill <dominicgs@gmail.com>
2018-12-04 17:31:56 -0700

    Try some cygwin workaround thing

--- firmware/appveyor.sh
+++ firmware/appveyor.sh
@@ -3,9 +3,10 @@
 pwd
 echo $PYTHONPATH
 which pip
+easy_install-2.7 pip
 /usr/bin/env python -V
 /usr/bin/env python -c "import sys; print sys.path"
-pip install --install-option="--prefix=c:\cygwin64/usr/lib/python2.7/site-packages" pyyaml
+/usr/bin/env python -m pip install --install-option="--prefix=/usr/lib/python2.7/site-packages" pyyaml
 ls /usr/lib/python2.7/site-packages
 cd firmware/libopencm3
 make
