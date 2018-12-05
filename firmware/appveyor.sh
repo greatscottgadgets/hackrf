f512cba916ee00b2a4ba972cd343bd99a0060c96
Dominic Spill <dominicgs@gmail.com>
2018-12-04 17:14:07 -0700

    Try a different invocation of pip

--- firmware/appveyor.sh
+++ firmware/appveyor.sh
@@ -4,7 +4,7 @@ pwd
 echo $PYTHONPATH
 /usr/bin/env python -V
 /usr/bin/env python -c "import sys; print sys.path"
-pip install --install-option="--prefix=/usr/lib/python2.7/site-packages" pyyaml
+/usr/bin/env python -m pip install --install-option="--prefix=/usr/lib/python2.7/site-packages" pyyaml
 # pip2 install pyyaml
 ls /usr/lib/python2.7/site-packages
 cd firmware/libopencm3
