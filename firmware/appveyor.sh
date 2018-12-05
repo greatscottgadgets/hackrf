7c127a574eeec90a98d8915a6b959bdcfdccd0bf
Dominic Spill <dominicgs@gmail.com>
2018-12-05 14:49:42 -0700

    Make make verbose (verbose)

--- firmware/appveyor.sh
+++ firmware/appveyor.sh
@@ -4,10 +4,9 @@
 /usr/bin/env python -m pip install pyyaml
 
 cd firmware/libopencm3
-make -h
-make lib/lpc43xx/m0
+make V=1 lib/lpc43xx/m0
 ls -l /cygdrive/c/projects/hackrf/firmware/libopencm3/lib/
-make lib/lpc43xx/m4
+make V=1 lib/lpc43xx/m4
 cd ..
 mkdir build-hackrf-one
 cd build-hackrf-one
