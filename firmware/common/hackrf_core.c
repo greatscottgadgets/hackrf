24fe561f3b20ec55b73bf2ce85e2404efb4f487a
Jared Boone <jboone@earfeast.com>
2019-02-01 21:47:12 +0000

    rad1o: Remove extra(?) SCU setup. Cut & paste oops?

--- firmware/common/hackrf_core.c
+++ firmware/common/hackrf_core.c
@@ -819,9 +819,6 @@ void pin_setup(void) {
 	gpio_output(&gpio_vaa_disable);
 
 #ifndef USER_INTERFACE_PORTAPACK
-	/* Not sure why this is necessary for stock HackRF. Just "rhyming" with the RAD1O code? */
-	scu_pinmux(SCU_PINMUX_GPIO3_10, SCU_GPIO_PDN | SCU_CONF_FUNCTION0);
-	scu_pinmux(SCU_PINMUX_GPIO3_11, SCU_GPIO_PDN | SCU_CONF_FUNCTION0);
 #endif
 #endif
 
