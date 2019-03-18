c5e9b55b14d7214b8f0aedea6b1cc136283a766f
Jared Boone <jboone@earfeast.com>
2019-03-18 16:22:14 -0700

    PortaPack: Refactor general UI functions into separate PortaPack API.

--- firmware/common/portapack.h
+++ firmware/common/portapack.h
@@ -22,12 +22,69 @@
 #ifndef __PORTAPACK_H__
 #define __PORTAPACK_H__
 
+#include <stddef.h>
+
 #include "hackrf-ui.h"
+
+#define ARRAY_SIZEOF(x) (sizeof(x) / sizeof(x[0]))
+
+typedef struct ui_color_t {
+	uint16_t v;
+} ui_color_t;
+
+typedef struct ui_point_t {
+	int16_t x;
+	int16_t y;
+} ui_point_t;
+
+typedef struct ui_size_t {
+	int16_t width;
+	int16_t height;
+} ui_size_t;
+
+typedef struct ui_rect_t {
+	ui_point_t point;
+	ui_size_t size;
+} ui_rect_t;
+
+typedef struct ui_bitmap_t {
+	ui_size_t size;
+	const uint8_t* const data;
+} ui_bitmap_t;
+
+typedef struct ui_font_t {
+	const ui_size_t glyph_size;
+	const uint8_t* const data;
+	char c_start;
+	size_t c_count;
+	size_t data_stride;
+} ui_font_t;
 
 typedef struct {
 	const hackrf_ui_t* const hackrf_ui;
 } portapack_t;
 
 const portapack_t* portapack_init(void);
+
+void portapack_backlight(const bool on);
+
+void portapack_fill_rectangle(
+	const ui_rect_t rect,
+	const ui_color_t color
+);
+
+void portapack_clear_display(const ui_color_t color);
+
+void portapack_draw_bitmap(
+	const ui_point_t point,
+	const ui_bitmap_t bitmap,
+	const ui_color_t foreground,
+	const ui_color_t background
+);
+
+ui_bitmap_t portapack_font_glyph(
+	const ui_font_t* const font,
+	const char c
+);
 
 #endif/*__PORTAPACK_H__*/
