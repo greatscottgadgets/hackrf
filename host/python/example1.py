74b0095e0dc49227c843f10fbd49b505e02fba61
Matioupi <mathieu.peyrega@gmail.com>
2019-10-20 14:53:03 +0200

    Add lib hackrf python wrapper and examples

---
+++ host/python/example1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+#
+
+from pyhackrf import *
+
+HackRF.setLogLevel(logging.INFO)
+#HackRF.setLogLevel(logging.DEBUG)
+
+result = HackRF.initialize()
+if (result != LibHackRfReturnCode.HACKRF_SUCCESS):
+    print("Erreur :",result, ",", HackRF.getHackRfErrorCodeName(result))
+
+theTestHackRF = HackRF()
+
+result = theTestHackRF.open()
+if (result != LibHackRfReturnCode.HACKRF_SUCCESS):
+    print("Erreur :",result, ",", HackRF.getHackRfErrorCodeName(result))
+
+theTestHackRF.printBoardInfos()
+
+result = theTestHackRF.close()
+if (result != LibHackRfReturnCode.HACKRF_SUCCESS):
+    print("Erreur :",result, ",", HackRF.getHackRfErrorCodeName(result))
+
+result = HackRF.deinitialize()
+if (result != LibHackRfReturnCode.HACKRF_SUCCESS):
+    print("Erreur :",result, ",", HackRF.getHackRfErrorCodeName(result))
+
+
