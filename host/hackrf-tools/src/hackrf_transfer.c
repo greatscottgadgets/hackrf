greatscottgadgets/hackrf

commit 8a9af7a1add97f82ed4b62d4d0b84e021e0a0219

hackrf_transfer: report on only actual transfers
The statistics reported to the user now reflect only completed USB
transfers and do not include information about the empty buffers that
are preloaded with data at the start of a TX operation.

host/hackrf-tools/src/hackrf_transfer.c
@@ -683,6 +683,7 @@ int main(int argc, char** argv)
 	unsigned int lna_gain = 8, vga_gain = 20, txvga_gain = 0;
 	hackrf_m0_state state;
 	stats_t stats = {0, 0};
+	static int32_t preload_bytes = 0;
 
 	while ((opt =
 			getopt(argc,
@@ -1272,6 +1273,8 @@ int main(int argc, char** argv)
 		.it_value = {.tv_sec = 1, .tv_usec = 0}};
 	setitimer(ITIMER_REAL, &interval_timer, NULL);
 #endif
+	preload_bytes = hackrf_get_transfer_queue_depth(device) *
+		hackrf_get_transfer_buffer_size(device);
 
 	while ((hackrf_is_streaming(device) == HACKRF_TRUE) && (do_exit == false)) {
 		uint64_t byte_count_now;
@@ -1323,6 +1326,23 @@ int main(int argc, char** argv)
 			stream_power_now = stream_power;
 			byte_count = 0;
 			stream_power = 0;
+
+			/*
+			 * The TX callback is called to preload the USB
+			 * transfer buffers at the start of TX. This results in
+			 * invalid statistics collected about the empty buffers
+			 * before any USB transfer is completed. We skip these
+			 * statistics and do not report them to the user.
+			 */
+			if (preload_bytes > 0) {
+				if (preload_bytes > byte_count_now) {
+					preload_bytes -= byte_count_now;
+					byte_count_now = 0;
+				} else {
+					byte_count_now -= preload_bytes;
+					preload_bytes = 0;
+				}
+			}
 
 			time_difference = TimevalDiff(&time_now, &time_start);
 			rate = (float) byte_count_now / time_difference;

commit f4202d1163aacd88c26714ea578f42da94fb91ee

hackrf_transfer: Don't skip preload stats in RX
Fixes a bug introduced in 8a9af7a: Statistics skipped for initial
buffers not only when preloaded in TX mode but also in RX mode with no
preloading.

The total bytes transferred may appear inflated in RX mode when
receiving a small number of samples with -n, but the stats represent
data transferred over USB, some of which may have been discarded by the
host.

host/hackrf-tools/src/hackrf_transfer.c
@@ -1249,6 +1249,8 @@ int main(int argc, char** argv)
 		result |= hackrf_set_lna_gain(device, lna_gain);
 		result |= hackrf_start_rx(device, rx_callback, NULL);
 	} else {
+		preload_bytes = hackrf_get_transfer_queue_depth(device) *
+			hackrf_get_transfer_buffer_size(device);
 		result = hackrf_set_txvga_gain(device, txvga_gain);
 		result |= hackrf_enable_tx_flush(device, 1);
 		result |= hackrf_start_tx(device, tx_callback, NULL);
@@ -1279,9 +1281,6 @@ int main(int argc, char** argv)
 		.it_value = {.tv_sec = 1, .tv_usec = 0}};
 	setitimer(ITIMER_REAL, &interval_timer, NULL);
 #endif
-	preload_bytes = hackrf_get_transfer_queue_depth(device) *
-		hackrf_get_transfer_buffer_size(device);
-
 	while ((hackrf_is_streaming(device) == HACKRF_TRUE) && (do_exit == false)) {
 		uint64_t byte_count_now;
 		struct timeval time_now;