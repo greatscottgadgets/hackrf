83f0437dde7b35160a77c0e8854707acc709dd81
Martin Ling <martin-git@earth.li>
2025-10-19 19:40:28 +0100

    Reduce memory transfer size to 8KB.

--- firmware/hackrf_usb/usb_api_transceiver.c
+++ firmware/hackrf_usb/usb_api_transceiver.c
@@ -48,7 +48,7 @@
 #include "usb_api_sweep.h"
 
 #define USB_TRANSFER_SIZE 0x4000
-#define DMA_TRANSFER_SIZE 0x4000
+#define DMA_TRANSFER_SIZE 0x2000
 
 #define BUF_HALF_MASK (USB_SAMP_BUFFER_SIZE >> 1)
 
