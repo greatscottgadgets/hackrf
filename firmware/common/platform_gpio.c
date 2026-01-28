6ebc5c85aa51e8a83ce8544dff93731c9c561cb1
Antoine van Gelder <antoine@greatscottgadgets.com>
2026-01-28 16:51:10 +0200

    firmware: use pre-defined gpio_t structs instead of GPIO() macro

--- firmware/common/platform_gpio.c
+++ firmware/common/platform_gpio.c
@@ -40,43 +40,43 @@ const platform_gpio_t* platform_gpio()
 	switch (board_id) {
 	case BOARD_ID_HACKRF1_OG:
 	case BOARD_ID_HACKRF1_R9:
-		gpio.hp             = GPIO(2, 0);
-		gpio.lp             = GPIO(2, 10);
-		gpio.tx_mix_bp      = GPIO(2, 11);
-		gpio.no_mix_bypass  = GPIO(1, 0);
-		gpio.rx_mix_bp      = GPIO(2, 12);
-		gpio.tx_amp         = GPIO(2, 15);
-		gpio.tx             = GPIO(5, 15);
-		gpio.mix_bypass     = GPIO(5, 16);
-		gpio.rx             = GPIO(5, 5);
-		gpio.no_tx_amp_pwr  = GPIO(3, 5);
-		gpio.amp_bypass     = GPIO(0, 14);
-		gpio.rx_amp         = GPIO(1, 11);
-		gpio.no_rx_amp_pwr  = GPIO(1, 12);
+		gpio.hp             = &GPIO2_0;
+		gpio.lp             = &GPIO2_10;
+		gpio.tx_mix_bp      = &GPIO2_11;
+		gpio.no_mix_bypass  = &GPIO1_0;
+		gpio.rx_mix_bp      = &GPIO2_12;
+		gpio.tx_amp         = &GPIO2_15;
+		gpio.tx             = &GPIO5_15;
+		gpio.mix_bypass     = &GPIO5_16;
+		gpio.rx             = &GPIO5_5;
+		gpio.no_tx_amp_pwr  = &GPIO3_5;
+		gpio.amp_bypass     = &GPIO0_14;
+		gpio.rx_amp         = &GPIO1_11;
+		gpio.no_rx_amp_pwr  = &GPIO1_12;
         // HackRF One rev 9
-		gpio.h1r9_rx         = GPIO(0, 7);
-		gpio.h1r9_no_ant_pwr = GPIO(2, 4);
+		gpio.h1r9_rx         = &GPIO0_7;
+		gpio.h1r9_no_ant_pwr = &GPIO2_4;
         break;
-    case BOARD_ID_RAD1O:
-		gpio.tx_rx_n         = GPIO(1, 11);
-		gpio.tx_rx           = GPIO(0, 14);
-		gpio.by_mix          = GPIO(1, 12);
-		gpio.by_mix_n        = GPIO(2, 10);
-		gpio.by_amp          = GPIO(1, 0);
-		gpio.by_amp_n        = GPIO(5, 5);
-		gpio.mixer_en        = GPIO(5, 16);
-		gpio.low_high_filt   = GPIO(2, 11);
-		gpio.low_high_filt_n = GPIO(2, 12);
-		gpio.tx_amp          = GPIO(2, 15);
-		gpio.rx_lna          = GPIO(5, 15);
+		case BOARD_ID_RAD1O:
+		gpio.tx_rx_n         = &GPIO1_11;
+		gpio.tx_rx           = &GPIO0_14;
+		gpio.by_mix          = &GPIO1_12;
+		gpio.by_mix_n        = &GPIO2_10;
+		gpio.by_amp          = &GPIO1_0;
+		gpio.by_amp_n        = &GPIO5_5;
+		gpio.mixer_en        = &GPIO5_16;
+		gpio.low_high_filt   = &GPIO2_11;
+		gpio.low_high_filt_n = &GPIO2_12;
+		gpio.tx_amp          = &GPIO2_15;
+		gpio.rx_lna          = &GPIO5_15;
         break;
     case BOARD_ID_PRALINE:
-		gpio.tx_en         = GPIO(3, 4);
-		gpio.mix_en_n      = GPIO(3, 2);
-		gpio.mix_en_n_r1_0 = GPIO(5, 6);
-		gpio.lpf_en        = GPIO(4, 8);
-		gpio.rf_amp_en     = GPIO(4, 9);
-		gpio.ant_bias_en_n = GPIO(1, 12);
+		gpio.tx_en         = &GPIO3_4;
+		gpio.mix_en_n      = &GPIO3_2;
+		gpio.mix_en_n_r1_0 = &GPIO5_6;
+		gpio.lpf_en        = &GPIO4_8;
+		gpio.rf_amp_en     = &GPIO4_9;
+		gpio.ant_bias_en_n = &GPIO1_12;
 		break;
 	case BOARD_ID_JELLYBEAN:
 	case BOARD_ID_JAWBREAKER:
