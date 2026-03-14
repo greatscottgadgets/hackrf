2d0b0872fe75f1dc0a6ae87e6e6f781381ad756e
Mike Walters <mike@flomp.net>
2026-03-14 16:26:03 +0000

    Skip detection of conflicting Opera Cake board if a DA7219 is detected

--- firmware/hackrf_usb/hackrf_usb.c
+++ firmware/hackrf_usb/hackrf_usb.c
@@ -63,6 +63,7 @@
 #include "fpga.h"
 #include "selftest.h"
 #include "delay.h"
+#include "da7219.h"
 
 extern uint32_t __m0_start__;
 extern uint32_t __m0_end__;
@@ -347,6 +348,9 @@ int main(void)
 	fpga_if_xcvr_selftest();
 #endif
 
+	if (da7219_detect()) {
+		operacake_skip_i2c_address(DA7219_ADDRESS);
+	}
 	bool operacake_allow_gpio;
 	if (hackrf_ui()->operacake_gpio_compatible()) {
 		operacake_allow_gpio = true;
