d862545ce3c78c7de1a5d6a0775dbac04e7f37ce
Martin Ling <martin-git@earth.li>
2026-04-04 01:09:30 +0100

    Universalize: hackrf_core.{c,h}, selftest.h
    Co-authored-by: Antoine van Gelder <antoine@greatscottgadgets.com>

--- firmware/common/hackrf_core.h
+++ firmware/common/hackrf_core.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 Great Scott Gadgets <info@greatscottgadgets.com>
+ * Copyright 2012-2026 Great Scott Gadgets <info@greatscottgadgets.com>
  * Copyright 2012 Benjamin Vernoux <titanmkd@gmail.com>
  * Copyright 2012 Jared Boone <jared@sharebrained.com>
  *
@@ -42,7 +42,7 @@ extern "C" {
 #include "si5351c.h"
 #include "spi_ssp.h"
 #include "w25q80bv.h"
-#if defined(PRALINE)
+#if defined(PRALINE) || defined(UNIVERSAL)
 	#include "fpga.h"
 	#include "ice40_spi.h"
 #endif
@@ -51,7 +51,8 @@ extern "C" {
 extern si5351c_driver_t clock_gen;
 extern ssp_config_t ssp_config_w25q80bv;
 
-#ifdef PRALINE
+extern max283x_driver_t max283x;
+#if defined(PRALINE) || defined(UNIVERSAL)
 extern ice40_spi_driver_t ice40;
 extern fpga_driver_t fpga;
 #endif
@@ -70,28 +71,27 @@ void clock_gen_init(void);
 void clock_gen_shutdown(void);
 void ssp1_set_mode_max283x(void);
 void ssp1_set_mode_max5864(void);
-#ifdef PRALINE
+#if defined(PRALINE) || defined(UNIVERSAL)
 void ssp1_set_mode_ice40(void);
 #endif
 
 void pin_shutdown(void);
 void pin_setup(void);
 
-#ifdef PRALINE
+#if defined(PRALINE) || defined(UNIVERSAL)
 void enable_1v2_power(void);
 void disable_1v2_power(void);
 void enable_3v3aux_power(void);
 void disable_3v3aux_power(void);
-#else
+#endif
 void enable_1v8_power(void);
 void disable_1v8_power(void);
-#endif
 
 fp_28_36_t sample_rate_set(const fp_28_36_t sample_rate, const bool program);
 
 clock_source_t activate_best_clock_source(void);
 
-#if (defined HACKRF_ONE || defined RAD1O || defined PRALINE)
+#if defined(RAD1O) || defined(HACKRF_ONE) || defined(PRALINE) || defined(UNIVERSAL)
 void enable_rf_power(void);
 void disable_rf_power(void);
 #endif
@@ -112,7 +112,7 @@ void trigger_enable(const bool enable);
 
 void halt_and_flash(const uint32_t duration);
 
-#ifdef PRALINE
+#if defined(PRALINE) || defined(UNIVERSAL)
 typedef enum {
 	P1_SIGNAL_TRIGGER_IN = 0,
 	P1_SIGNAL_AUX_CLK1 = 1,
