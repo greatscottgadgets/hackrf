6bf6198b309d00e93d169e7eb0875bdd6e730d0e
Michael Ossmann <mike@ossmann.com>
2026-04-09 14:27:27 -0400

    Refactor radio tuning
    - Store applied settings in radio registers
    - Support arbitrary combinations of tuning registers

--- firmware/common/tuning.h
+++ firmware/common/tuning.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 Great Scott Gadgets <info@greatscottgadgets.com>
+ * Copyright 2012-2026 Great Scott Gadgets <info@greatscottgadgets.com>
  * Copyright 2012 Jared Boone
  * Copyright 2013 Benjamin Vernoux
  *
@@ -23,28 +23,20 @@
 
 #pragma once
 
-#include <stdbool.h>
-#include <stdint.h>
-
+#include "fixed_point.h"
 #include "rf_path.h"
-#include "transceiver_mode.h"
-#if defined(PRALINE)
-	#include "tune_config.h"
-#endif
 
-#define FREQ_ONE_MHZ (1000ULL * 1000)
+/**
+ * Select fixed or graduated IF for a given RF and image reject filter.
+ */
+fp_40_24_t select_graduated_if(fp_40_24_t freq_rf, rf_path_filter_t img_reject);
 
-bool set_freq(const uint64_t freq, const transceiver_mode_t opmode);
-bool set_freq_explicit(
-	const uint64_t if_freq_hz,
-	const uint64_t lo_freq_hz,
-	const rf_path_filter_t path,
-	const transceiver_mode_t opmode);
+/**
+ * Select image reject filter appropriate for a given RF.
+ */
+rf_path_filter_t select_img_reject(fp_40_24_t freq_rf);
 
-#ifdef PRALINE
-bool tuning_set_frequency(
-	const tune_config_t* cfg,
-	const uint64_t freq,
-	const uint32_t offset,
-	const transceiver_mode_t opmode);
-#endif
+/**
+ * Restrict RF to range appropriate for a given image reject filter.
+ */
+fp_40_24_t restrict_rf(fp_40_24_t freq_rf, rf_path_filter_t img_reject);
