4366b492069ee03a946169ee221f27632fe2a154
Michael Ossmann <mike@ossmann.com>
2026-04-09 14:27:27 -0400

    Refactor radio tuning
    - Store applied settings in radio registers
    - Support arbitrary combinations of tuning registers

--- firmware/common/radio.c
+++ firmware/common/radio.c
@@ -28,14 +28,19 @@
 #include "fixed_point.h"
 #include "hackrf_core.h"
 #include "hackrf_ui.h"
+#include "mixer.h"
 #include "max283x.h"
 #include "platform_detect.h"
 #include "rf_path.h"
+#include "sgpio.h"
 #include "transceiver_mode.h"
 #include "tuning.h"
 #if defined(PRALINE)
 	#include "fpga.h"
 	#include "tune_config.h"
+#endif
+#if defined(HACKRF_ONE) || defined(PRALINE)
+	#include "operacake.h"
 #endif
 
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
@@ -240,139 +245,316 @@ static bool radio_update_sample_rate(radio_t* const radio, uint64_t* bank)
 	return (new_afe_rate || new_rate || new_n);
 }
 
-#define DEFAULT_RF FP_MHZ(2450)
-
-static fp_28_36_t applied_offset = RADIO_UNSET;
-
-#ifdef PRALINE
-static const tune_config_t* select_tune_config(uint64_t opmode)
+/*
+ * Compute frequency offset in 1/(2**24) Hz for a given rotation. This is an
+ * unsigned value that does not indicate direction of frequency shift.
+ */
+static fp_40_24_t compute_offset(radio_t* const radio, uint64_t opmode, uint64_t rotation)
 {
+	uint64_t n;
+	fp_40_24_t offset = 0;
+	const fp_28_36_t mcu_rate = radio->config[RADIO_BANK_APPLIED][RADIO_SAMPLE_RATE];
+
 	switch (opmode) {
 	case TRANSCEIVER_MODE_TX:
 	case TRANSCEIVER_MODE_SS:
-		return praline_tune_config_tx;
-	case TRANSCEIVER_MODE_RX_SWEEP:
-		return praline_tune_config_rx_sweep;
+		n = radio->config[RADIO_BANK_APPLIED][RADIO_RESAMPLE_TX];
+		break;
 	default:
-		return praline_tune_config_rx;
+		n = radio->config[RADIO_BANK_APPLIED][RADIO_RESAMPLE_RX];
 	}
+	if ((rotation != 0) && (rotation != RADIO_UNSET) && (mcu_rate != RADIO_UNSET) &&
+	    (n != RADIO_UNSET)) {
+		const fp_40_24_t afe_rate = mcu_rate >> (12 - n);
+		if (rotation & 0x80) {
+			rotation = 0x100 - rotation;
+		}
+		offset = (afe_rate * rotation) >> 8;
+	}
+	return offset;
+}
+
+/*
+ * Convert center frequency of analog baseband (as seen by ADC/DAC) to center
+ * frequency of digital baseband (as seen by MCU).
+ */
+static fp_40_24_t digital_from_analog_rf(
+	radio_t* const radio,
+	const fp_40_24_t arf,
+	uint64_t opmode,
+	uint64_t rotation)
+{
+	fp_40_24_t offset = compute_offset(radio, opmode, rotation);
+	fp_40_24_t drf = arf;
+	if (rotation > 0x80) {
+		if (offset > arf) {
+			drf = offset - arf;
+		} else {
+			drf = arf - offset;
+		}
+	} else if ((rotation & 0x7f) > 0x00) {
+		drf = arf + offset;
+	}
+	return drf;
+}
+
+#ifdef PRALINE
+/*
+ * Convert center frequency of digital baseband (as seen by MCU) to center
+ * frequency of analog baseband (as seen by ADC/DAC).
+ */
+static fp_40_24_t analog_from_digital_rf(
+	radio_t* const radio,
+	const fp_40_24_t drf,
+	uint64_t opmode,
+	uint64_t rotation)
+{
+	fp_40_24_t offset = compute_offset(radio, opmode, rotation);
+	fp_40_24_t arf = drf;
+	if (rotation > 0x80) {
+		arf = drf + offset;
+	} else if ((rotation & 0x7f) > 0x00) {
+		if (offset > drf) {
+			arf = offset - drf;
+		} else {
+			arf = drf - offset;
+		}
+	}
+	return arf;
+}
+
+static const tune_config_t* select_tune_config(uint64_t opmode, fp_40_24_t freq_rf)
+{
+	const tune_config_t* tune_config;
+
+	switch (opmode) {
+	case TRANSCEIVER_MODE_TX:
+	case TRANSCEIVER_MODE_SS:
+		tune_config = praline_tune_config_tx;
+		break;
+	case TRANSCEIVER_MODE_RX_SWEEP:
+		tune_config = praline_tune_config_rx_sweep;
+		break;
+	default:
+		tune_config = praline_tune_config_rx;
+	}
+	while ((tune_config->rf_range_end_mhz != 0) || (tune_config->if_mhz != 0)) {
+		if ((freq_rf == 0) ||
+		    (tune_config->rf_range_end_mhz > (freq_rf / FP_ONE_MHZ))) {
+			break;
+		}
+		tune_config++;
+	}
+	return tune_config;
 }
 #endif
 
 static bool radio_update_frequency(radio_t* const radio, uint64_t* bank)
 {
-	uint8_t rotation = 0;
-	uint64_t requested_rf = bank[RADIO_FREQUENCY_RF];
+	bool new_freq = false;
+	bool high_lo = false;
+	bool invert_spectrum = false;
+
+	const uint64_t requested_rf = bank[RADIO_FREQUENCY_RF];
 	const uint64_t requested_if = bank[RADIO_FREQUENCY_IF];
 	const uint64_t requested_lo = bank[RADIO_FREQUENCY_LO];
 	const uint64_t requested_img_reject = bank[RADIO_IMAGE_REJECT];
+	const uint64_t requested_rotation = bank[RADIO_ROTATION];
 
-	bool set_if = (requested_if != RADIO_UNSET);
-	bool set_lo = (requested_lo != RADIO_UNSET);
-	bool set_img_reject = (requested_img_reject != RADIO_UNSET);
+	const uint64_t applied_rf = radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_RF];
+	const uint64_t applied_if = radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_IF];
+	const uint64_t applied_lo = radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_LO];
+	const uint64_t applied_img_reject =
+		radio->config[RADIO_BANK_APPLIED][RADIO_IMAGE_REJECT];
+	const uint64_t applied_rotation =
+		radio->config[RADIO_BANK_APPLIED][RADIO_ROTATION];
+
+	uint64_t freq_rf = applied_rf;
+	uint64_t analog_rf = applied_rf;
+	uint64_t freq_if = applied_if;
+	uint64_t freq_lo = applied_lo;
+	uint64_t img_reject = applied_img_reject;
+	uint64_t rotation = applied_rotation;
 
 	uint64_t opmode = bank[RADIO_OPMODE];
 	if (opmode == RADIO_UNSET) {
 		opmode = radio->config[RADIO_BANK_APPLIED][RADIO_OPMODE];
 	}
 
-	if (set_if && set_lo && set_img_reject) {
-		bool new_if =
-			(requested_if !=
-			 radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_IF]);
-		bool new_lo =
-			(requested_lo !=
-			 radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_LO]);
-		bool new_img_reject =
-			(requested_img_reject !=
-			 radio->config[RADIO_BANK_APPLIED][RADIO_IMAGE_REJECT]);
-		if (new_if || new_lo || new_img_reject) {
-			set_freq_explicit(
-				requested_if / FP_ONE_HZ,
-				requested_lo / FP_ONE_HZ,
-				requested_img_reject,
-				opmode);
-			radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_IF] =
-				requested_if;
-			radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_LO] =
-				requested_lo;
-			radio->config[RADIO_BANK_APPLIED][RADIO_IMAGE_REJECT] =
-				requested_img_reject;
-			radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_RF] =
-				RADIO_UNSET;
+	/* Restrict requested settings to valid ranges. */
+	if (requested_img_reject != RADIO_UNSET) {
+		switch (requested_img_reject) {
+		case RF_PATH_FILTER_LOW_PASS:
+		case RF_PATH_FILTER_HIGH_PASS:
+			img_reject = requested_img_reject;
+			break;
+		default:
+			img_reject = RF_PATH_FILTER_BYPASS;
 		}
-#ifdef PRALINE
-		const uint64_t requested_rotation = bank[RADIO_ROTATION];
+	}
+	if (requested_if != RADIO_UNSET) {
+		freq_if = max283x_set_frequency(&max283x, requested_if, false);
+	}
+	if (requested_lo != RADIO_UNSET) {
+		freq_lo = mixer_set_frequency(&mixer, freq_lo, false);
+	}
+	if (detected_platform() == BOARD_ID_PRALINE) {
 		if (requested_rotation != RADIO_UNSET) {
 			rotation = requested_rotation;
-		} else if (radio->config[RADIO_BANK_APPLIED][RADIO_ROTATION] != RADIO_UNSET) {
-			return true;
 		}
+		switch (opmode) {
+		case TRANSCEIVER_MODE_RX:
+		case TRANSCEIVER_MODE_RX_SWEEP:
+			/* Round to nearest supported rotation. */
+			rotation = (rotation + 0x20) & 0xc0;
+			if (rotation == 0x80) {
+				rotation = 0;
+			}
+			break;
+		default:
+			rotation = 0;
+		}
+	} else {
+		rotation = 0;
+	}
+
+	/* Handle requested RF (auto-tune). */
+	if (requested_rf != RADIO_UNSET) {
+		if (requested_img_reject == RADIO_UNSET) {
+			img_reject = select_img_reject(requested_rf);
+		}
+		freq_rf = restrict_rf(requested_rf, img_reject);
+
+		/* Look up settings appropriate for requested RF. */
+		if (detected_platform() == BOARD_ID_PRALINE) {
+#ifdef PRALINE
+			const tune_config_t* tune_config =
+				select_tune_config(opmode, freq_rf);
+			if (requested_rotation == RADIO_UNSET) {
+				rotation = (uint8_t) (tune_config->shift) << 6;
+			}
+			analog_rf =
+				analog_from_digital_rf(radio, freq_rf, opmode, rotation);
+			if (requested_if == RADIO_UNSET) {
+				if (tune_config->if_mhz == 0) {
+					freq_if = analog_rf;
+				} else {
+					freq_if = tune_config->if_mhz * FP_ONE_MHZ;
+				}
+			}
+			high_lo = tune_config->high_lo;
+#endif
+		} else {
+			/* Use graduated or fixed IF on older platforms. */
+			if (requested_if == RADIO_UNSET) {
+				freq_if = select_graduated_if(freq_rf, img_reject);
+			}
+			high_lo = (img_reject == RF_PATH_FILTER_LOW_PASS);
+			analog_rf = freq_rf;
+		}
+
+		/* Compute precise LO. This is done first because the mixer LO is set in coarse steps. */
+		if (requested_lo == RADIO_UNSET) {
+			switch (img_reject) {
+			case RF_PATH_FILTER_LOW_PASS:
+				if (high_lo) {
+					freq_lo = freq_if + analog_rf;
+				} else {
+					freq_lo = freq_if - analog_rf;
+				}
+				break;
+			case RF_PATH_FILTER_HIGH_PASS:
+				freq_lo = analog_rf - freq_if;
+				break;
+			default:
+				freq_lo = RADIO_UNSET;
+			}
+			if (freq_lo != RADIO_UNSET) {
+				freq_lo = mixer_set_frequency(&mixer, freq_lo, false);
+			}
+		}
+
+		/* Recompute IF, compensating for rounding of mixer LO. */
+		if (requested_if == RADIO_UNSET) {
+			switch (img_reject) {
+			case RF_PATH_FILTER_LOW_PASS:
+				if (high_lo) {
+					freq_if = freq_lo - analog_rf;
+				} else {
+					freq_if = freq_lo + analog_rf;
+				}
+				break;
+			case RF_PATH_FILTER_HIGH_PASS:
+				freq_if = analog_rf - freq_lo;
+			}
+		}
+	}
+
+	/* Apply settings. */
+	if ((freq_if != applied_if) && (freq_if != RADIO_UNSET)) {
+		freq_if = max283x_set_frequency(&max283x, freq_if, true);
+		radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_IF] = freq_if;
+		new_freq = true;
+	}
+	if ((freq_lo != applied_lo) && (freq_lo != RADIO_UNSET)) {
+		freq_lo = mixer_set_frequency(&mixer, freq_lo, true);
+		radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_LO] = freq_lo;
+		new_freq = true;
+	}
+	if ((img_reject != applied_img_reject) && (img_reject != RADIO_UNSET)) {
+		rf_path_set_filter(&rf_path, img_reject, opmode);
+		radio->config[RADIO_BANK_APPLIED][RADIO_IMAGE_REJECT] = img_reject;
+		new_freq = true;
+	}
+	if ((rotation != applied_rotation) && (rotation != RADIO_UNSET)) {
+#ifdef PRALINE
 		fpga_set_rx_quarter_shift_mode(&fpga, rotation >> 6);
 #endif
 		radio->config[RADIO_BANK_APPLIED][RADIO_ROTATION] = rotation;
-		return true;
+		new_freq = true;
 	}
 
-	if (requested_rf == RADIO_UNSET) {
-		if (radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_RF] ==
-		    RADIO_UNSET) {
-			requested_rf = DEFAULT_RF;
-		} else {
-			return false;
-		}
-	}
-
-	bool new_rf =
-		(radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_RF] != requested_rf);
-	uint64_t requested_rf_hz = requested_rf / FP_ONE_HZ;
-#ifdef PRALINE
-	if (applied_afe_rate == RADIO_UNSET) {
-		return false;
-	}
-	const tune_config_t* tune_config = select_tune_config(opmode);
-	const tune_config_t* applied_tune_config =
-		select_tune_config(radio->config[RADIO_BANK_APPLIED][RADIO_OPMODE]);
-	bool new_config = (applied_tune_config != tune_config);
-	while ((tune_config->rf_range_end_mhz != 0) || (tune_config->if_mhz != 0)) {
-		if ((requested_rf_hz == 0) ||
-		    (tune_config->rf_range_end_mhz > (requested_rf_hz / FREQ_ONE_MHZ))) {
+	/* Compute precise RF. */
+	if ((img_reject != RADIO_UNSET) && (freq_if != RADIO_UNSET) &&
+	    (rotation != RADIO_UNSET) &&
+	    ((freq_lo != RADIO_UNSET) || (img_reject == RF_PATH_FILTER_BYPASS))) {
+		switch (img_reject) {
+		case RF_PATH_FILTER_LOW_PASS:
+			if (freq_lo > freq_if) {
+				analog_rf = freq_lo - freq_if;
+				invert_spectrum = true;
+			} else {
+				analog_rf = freq_if - freq_lo;
+			}
 			break;
+		case RF_PATH_FILTER_HIGH_PASS:
+			analog_rf = freq_if + freq_lo;
+			break;
+		default:
+			analog_rf = freq_if;
 		}
-		tune_config++;
+		freq_rf = digital_from_analog_rf(radio, analog_rf, opmode, rotation);
 	}
-	bool new_rotation =
-		(radio->config[RADIO_BANK_APPLIED][RADIO_ROTATION] !=
-		 ((uint64_t) tune_config->shift << 6));
-	if (new_rotation) {
-		fpga_set_rx_quarter_shift_mode(&fpga, tune_config->shift);
-		radio->config[RADIO_BANK_APPLIED][RADIO_ROTATION] =
-			tune_config->shift << 6;
+	if ((freq_rf != applied_rf) && (freq_rf != RADIO_UNSET)) {
+		radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_RF] = freq_rf;
+		new_freq = true;
 	}
-	fp_28_36_t offset = applied_afe_rate / 4;
-	bool new_offset = (applied_offset != offset);
-	if (new_rotation || new_offset || new_config || new_rf) {
-		tuning_set_frequency(
-			tune_config,
-			requested_rf_hz,
-			offset / SR_FP_ONE_HZ,
-			opmode);
-		applied_offset = offset;
-	}
-#else
-	if (new_rf) {
-		set_freq(requested_rf_hz, opmode);
-	}
+
+	if (new_freq) {
+		sgpio_cpld_set_mixer_invert(&sgpio_config, invert_spectrum);
+		if (opmode != TRANSCEIVER_MODE_RX_SWEEP) {
+			hackrf_ui()->set_frequency(freq_rf / FP_ONE_MHZ);
+		}
+#if defined(PRALINE) || defined(HACKRF_ONE)
+		operacake_set_range(freq_rf / FP_ONE_MHZ);
 #endif
-	radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_RF] = requested_rf;
-	radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_IF] = RADIO_UNSET;
-	radio->config[RADIO_BANK_APPLIED][RADIO_FREQUENCY_LO] = RADIO_UNSET;
-	radio->config[RADIO_BANK_APPLIED][RADIO_IMAGE_REJECT] = RADIO_UNSET;
-	return true;
+	}
+	return new_freq;
 }
 
-static uint32_t auto_bandwidth(radio_t* const radio)
+static uint32_t auto_bandwidth(radio_t* const radio, uint64_t opmode)
 {
+	uint32_t offset_hz = 0;
 	uint64_t rotation = radio->config[RADIO_BANK_APPLIED][RADIO_ROTATION];
 
 	uint32_t sample_rate_hz = DEFAULT_MCU_RATE / SR_FP_ONE_HZ;
@@ -381,11 +563,8 @@ static uint32_t auto_bandwidth(radio_t* const radio)
 			SR_FP_ONE_HZ;
 	}
 
-	uint32_t offset_hz = 0;
-	if ((rotation != 0) && (rotation != RADIO_UNSET) &&
-	    (applied_offset != RADIO_UNSET)) {
-		offset_hz = applied_offset / SR_FP_ONE_HZ;
-	}
+	const fp_40_24_t offset = compute_offset(radio, opmode, rotation);
+	offset_hz = offset >> 24;
 
 	const uint32_t bb_bandwidth = (sample_rate_hz * 3) / 4;
 	const uint32_t lpf_bandwidth = bb_bandwidth + offset_hz * 2;
@@ -400,10 +579,15 @@ static bool radio_update_bandwidth(radio_t* const radio, uint64_t* bank)
 {
 	bool new_bw = false;
 
+	uint64_t opmode = bank[RADIO_OPMODE];
+	if (opmode == RADIO_UNSET) {
+		opmode = radio->config[RADIO_BANK_APPLIED][RADIO_OPMODE];
+	}
+
 #ifdef PRALINE
 	/* Praline legacy mode always sets baseband bandwidth automatically. */
 	(void) bank;
-	uint32_t lpf_bandwidth = auto_bandwidth(radio);
+	uint32_t lpf_bandwidth = auto_bandwidth(radio, opmode);
 
 	if (radio->config[RADIO_BANK_APPLIED][RADIO_XCVR_TX_LPF] != lpf_bandwidth) {
 		max283x_set_lpf_bandwidth(&max283x, MAX283x_MODE_TX, lpf_bandwidth);
@@ -450,7 +634,7 @@ static bool radio_update_bandwidth(radio_t* const radio, uint64_t* bank)
 		lpf_bandwidth = radio->config[RADIO_BANK_APPLIED][RADIO_XCVR_TX_LPF];
 	}
 	if (lpf_bandwidth == RADIO_UNSET) {
-		lpf_bandwidth = auto_bandwidth(radio);
+		lpf_bandwidth = auto_bandwidth(radio, opmode);
 	}
 
 	if (radio->config[RADIO_BANK_APPLIED][RADIO_XCVR_TX_LPF] != lpf_bandwidth) {
