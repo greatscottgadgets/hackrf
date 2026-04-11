1b06ccf0f7b9ecd8b0715e6550d3d9d3a4122cee
Martin Ling <martin-git@earth.li>
2026-04-11 23:46:55 +0100

    Add macros to clarify fixed-point constants.

--- firmware/common/fixed_point.h
+++ firmware/common/fixed_point.h
@@ -29,8 +29,15 @@ typedef uint64_t fp_40_24_t;
 /* one million in 40.24 fixed-point */
 #define FP_ONE_MHZ ((1000ULL * 1000ULL) << 24)
 
+/* one thousand in 40.24 fixed-point */
+#define FP_ONE_KHZ ((1000ULL) << 24)
+
 /* one in 40.24 fixed-point */
 #define FP_ONE_HZ (1 << 24)
+
+#define FP_MHZ(mhz) (mhz##ULL * FP_ONE_MHZ)
+#define FP_KHZ(khz) (khz##ULL * FP_ONE_KHZ)
+#define FP_HZ(hz)   (hz##ULL * FP_ONE_HZ)
 
 /* 28.36 fixed-point */
 typedef uint64_t fp_28_36_t;
@@ -38,5 +45,12 @@ typedef uint64_t fp_28_36_t;
 /* one million in 28.36 fixed-point */
 #define SR_FP_ONE_MHZ ((1000ULL * 1000ULL) << 36)
 
+/* one thousand in 28.36 fixed-point */
+#define SR_FP_ONE_KHZ (1000ULL << 36)
+
 /* one in 28.36 fixed-point */
 #define SR_FP_ONE_HZ (1ULL << 36)
+
+#define SR_FP_MHZ(mhz) (mhz##ULL * SR_FP_ONE_MHZ)
+#define SR_FP_KHZ(khz) (khz##ULL * SR_FP_ONE_KHZ)
+#define SR_FP_HZ(hz)   (hz##ULL * SR_FP_ONE_HZ)
