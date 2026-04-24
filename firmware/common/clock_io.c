8ef5ae5020573f8c84592b1122575d3ec017cf83
Antoine van Gelder <antoine@greatscottgadgets.com>
2026-04-24 12:09:19 +0200

    firmware: alternate solution to 'pragma: keep'

--- firmware/common/clock_io.c
+++ firmware/common/clock_io.c
@@ -33,10 +33,12 @@
 #include "gpdma.h"
 #include "gpio.h"
 #include "hackrf_core.h"
-#include "platform_detect.h" // IWYU pragma: keep (universal)
 #ifdef IS_PRALINE
 	#include "fpga.h"
 	#include "platform_gpio.h"
+#endif
+#ifdef IS_UNIVERSAL
+	#include "platform_detect.h"
 #endif
 
 #define CLOCK_CYCLES_1_MS     (204000)
