813d4da05ef5b68bf020d843efefa3413aaca83f
Alba Mendez <me@alba.sh>
2026-07-05 02:23:30 +0200

    make convergent_round work with no integer bits
    convergent_round returns a value that is one bit longer than the non-fractional part of the passed value. it makes sense to pass a value that is exactly `discarded_bits` long IF it is unsigned. make convergent_round tolerate this edge case rather than fail, but make sure it still fails if `value` has less than `discarded_bits` non-sign bits.
    
    more practically, this fixes an error when assigning a fixed.Value of i_width=0 and f_width > 0 to another with f_width=0

--- firmware/fpga/dsp/round.py
+++ firmware/fpga/dsp/round.py
@@ -5,13 +5,20 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 def convergent_round(value, discarded_bits):
+    shape = value.shape()
+    if discarded_bits > shape.width - shape.signed:
+        raise ValueError(f'cannot discard {discarded_bits} bits from a value with {shape.width - shape.signed} non-sign bits')
     retained = value[discarded_bits:]
     discarded = value[:discarded_bits]
     msb_discarded = discarded[-1]
     rest_discarded = discarded[:-1]
-    lsb_retained = retained[0]
+    lsb_retained = retained[0] if len(retained) else 0
     # Round up:
     # - If discarded > 0.5
     # - If discarded == 0.5 and retained is odd
     round_up = msb_discarded & (rest_discarded.any() | lsb_retained)
+    if len(retained) == 0:
+        # the returned result's width should always be 1 more than retained,
+        # and Amaranth would produce 2 bits if we did the addition
+        return round_up
     return retained + round_up
