c7420af4d448ac43a8005b80425177c44e71179c
Martin Ling <martin-git@earth.li>
2026-08-21 16:33:19 +0100

    Get bitstream address from firmware info struct at start of flash.
    This allows a firmware that has been embedded inside a larger image
    to determine where it should find its bitstream in the flash.

--- firmware/hackrf_usb/hackrf_usb.c
+++ firmware/hackrf_usb/hackrf_usb.c
@@ -65,8 +65,8 @@
 #ifdef IS_PRALINE
 	#include <fpga.h>
 	#if !(defined(DFU_MODE) || defined(RAM_MODE))
+		#include <firmware_info.h>
 		#include <lz4_buf.h>
-		#include <spi_bus.h>
 		#include <w25q80bv.h>
 	#endif
 #endif
@@ -271,11 +271,31 @@ static void m0_rom_to_ram(void)
 }
 
 #if defined(IS_PRALINE) && !(defined(DFU_MODE) || defined(RAM_MODE))
-extern uint32_t _binary_fpga_bin_start;
+
+bool fpga_loader_setup(void);
+bool fpga_loader_read(uint32_t addr, uint32_t size, uint8_t* buf);
+
+struct fpga_loader_t fpga_loader = {
+	.start_addr = 0,
+	.setup = fpga_loader_setup,
+	.read = fpga_loader_read,
+	.in_buffer = lz4_in_buf,
+	.out_buffer = lz4_out_buf,
+};
 
 bool fpga_loader_setup(void)
 {
 	w25q80bv_setup(&spi_flash);
+
+	/* Read firmware info from start of flash, after vector table */
+	struct firmware_info_t info;
+	w25q80bv_read(&spi_flash, 0x400, sizeof(info), (uint8_t*) &info);
+
+	if (memcmp(info.bitstream_magic, "FPGABITS", 8)) {
+		return false;
+	}
+
+	fpga_loader.start_addr = info.bitstream_flash_addr;
 	return true;
 }
 
@@ -285,13 +305,6 @@ bool fpga_loader_read(uint32_t addr, uint32_t size, uint8_t* buf)
 	return true;
 }
 
-struct fpga_loader_t fpga_loader = {
-	.start_addr = (uint32_t) &_binary_fpga_bin_start,
-	.setup = fpga_loader_setup,
-	.read = fpga_loader_read,
-	.in_buffer = lz4_in_buf,
-	.out_buffer = lz4_out_buf,
-};
 #endif
 
 void radio_changed(const uint32_t changed)
